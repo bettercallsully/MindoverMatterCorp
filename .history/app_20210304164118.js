0a61775d08df9faaa1918e1143c14d1a6e73806e
bettercallsully <solomonelkelani@gmail.com>
2021-03-04 21:49:22 -0600

    User authentication started, product page fixed

---
+++ .history/app_20210304164118.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+
+var session = require('express-session');
+
+// config express-session
+var sess = {
+  secret: 'CHANGE THIS TO A RANDOM SECRET',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  app.get('/', (req, res) => {
+    MongoClient.connect(url, function(err, client) {
+      const db = client.db('#');
+      const collection = db.collection('#');
+  
+      collection.find({}).toArray((error, documents) => {
+        client.close();
+        res.render('index', { documents: documents });
+      });
+    });
+  });
+});
+
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+  res.render('home', {})
+});
+
+app.use(express.static('public',));
+
+app.get('/', (req, res) => {
+  
+})
+
---
+++ .history/app_20210304164229.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+
+var session = require('express-session');
+
+// config express-session
+var sess = {
+  secret: 'CHANGE THIS TO A RANDOM SECRET',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
+
+// Load environment variables from .env
+var dotenv = require('dotenv');
+dotenv.config();
+
+// Load Passport
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:5500/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  app.get('/', (req, res) => {
+    MongoClient.connect(url, function(err, client) {
+      const db = client.db('#');
+      const collection = db.collection('#');
+  
+      collection.find({}).toArray((error, documents) => {
+        client.close();
+        res.render('index', { documents: documents });
+      });
+    });
+  });
+});
+
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+  res.render('home', {})
+});
+
+app.use(express.static('public',));
+
+app.get('/', (req, res) => {
+  
+})
+

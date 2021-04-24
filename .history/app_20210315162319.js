1971670c1648170fe38917328aea1d8a965a8e92
bettercallsully <solomonelkelani@gmail.com>
2021-04-23 22:03:44 -0600

    Add CSS updates, and complete Products page

---
+++ .history/app_20210315162319.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const express = require('mongodb')
+const app = express();
+const port = 5500;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  app.get('/', (req, res) => {
+    MongoClient.connect(url, function(err, client) {
+      const db = client.db('#');
+      const collection = db.collection('#');
+  
+      collection.find({}).toArray((error, documents) => {
+        client.close();
+        res.render('index', { documents: documents });
+      });
+    });
+  });
+});
+
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+  res.render('home', {})
+});
+
+
---
+++ .history/app_20210315164610.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+
+//MongoDB Connection Link
+const url = 'mongod://localhost:27017';
+
+//Database Name
+const dbName = 'datacollections'; 
+
+//Mongo Client
+const client = new MongoClient(url);
+
+//Connection method to connect to the server
+client.connect(function(err)) {
+  assert.equal(null, err); 
+  console.log("Connected successfully to server");
+
+  const db = 
+}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  app.get('/', (req, res) => {
+    MongoClient.connect(url, function(err, client) {
+      const db = client.db('#');
+      const collection = db.collection('#');
+  
+      collection.find({}).toArray((error, documents) => {
+        client.close();
+        res.render('index', { documents: documents });
+      });
+    });
+  });
+});
+
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+  res.render('home', {})
+});
+
+
